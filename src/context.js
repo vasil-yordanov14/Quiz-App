9ad868086a70560d071d94a9f8fbdce0218efd4c
vasil-yordanov14 <vasil_sh@abv.bg>
2022-08-21 13:50:17 +0300

    setup form

--- src/context.js
+++ src/context.js
@@ -20,6 +20,11 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: `sports`,
+    difficulty: `easy`,
+  });
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -70,9 +75,12 @@ const AppProvider = ({ children }) => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    fetchQuestions(tempUrl);
-  }, []);
+  const handleChange = (e) => {
+    console.log(e);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <AppContext.Provider
@@ -87,6 +95,9 @@ const AppProvider = ({ children }) => {
         nextQuestion,
         checkAnswer,
         closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
       }}
     >
       {children}
